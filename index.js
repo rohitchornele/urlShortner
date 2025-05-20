03ed57c56f21c4f13ccc8abf6c2cc563e4b13e9e
Rohit Chornele <rohitchornele@gmail.com>
2025-05-20 21:01:41 +0530

    Correcting for deployment 3

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ app.get("/shorten", (req, res) => {
   res.redirect('/');
 });
 
+
 //redirect to original url using short url\
 app.get('/:shortCode', getOriginalUrl)
 
-// app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
